cb2e4ed50ebe555f143fa2fc99e296a041262d27
Ray Chung <ray.j.chung@gmail.com>
2020-10-21 02:36:52 -0700

    Add /api/users/currentuser route and morgan logging.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import { json } from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 app.use(json());
+app.use(morgan("dev"))
+
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi!');
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on 3000...');
