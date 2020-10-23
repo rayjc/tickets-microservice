0c06d87472d3c78fcc5c3c8be0d51723bc8709fa
Ray Chung <ray.j.chung@gmail.com>
2020-10-23 02:20:43 -0700

    Add mongodb setup to auth service; add mongo deployment config.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,17 +1,49 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/NotFoundError';
 
 const app = express();
 app.use(json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi!');
+// 404 error
+app.all('*', async () => {
+  throw new NotFoundError();
 });
+// error handler
+app.use(errorHandler);
+
+const init = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (error) {
+    console.error(error);
+  }
 
 
-app.listen(3000, () => {
-  console.log('Listening on 3000...');
-});
+  app.listen(3000, () => {
+    console.log('Listening on 3000...');
+  });
+};
+
+
+
+init();
