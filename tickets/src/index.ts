d4da9c81ba9bb28aba87ebc1078719c1eddf6bad
Ray Chung <ray.j.chung@gmail.com>
2020-11-13 17:01:10 -0800

    Add NatsWrapper helper class; add TicketCreatedPublisher; update POST /api/tickets to publish event.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,9 +2,20 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import { MONGODB_URI } from './config';
+import { natsWrapper } from './NatsWrapper';
 
 const init = async () => {
   try {
+    await natsWrapper.connect('ticketing', 'some_id', 'http://nats-srv:4222');
+    // could consider moving clean up to a natsWrapper method
+    // though prefer to exit process at top level
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@rayjc-dev/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/TicketCreatedPublisher';
+import { natsWrapper } from '../NatsWrapper';
 
 const router = express.Router();
 
@@ -21,6 +23,13 @@ router.post('/api/tickets',
       userId: req.currentUser!.id
     });
     await ticket.save();
+    // publish sanitized/saved data
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).json(ticket);
   }
