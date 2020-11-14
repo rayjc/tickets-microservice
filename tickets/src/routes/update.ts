75bc3d55e5757ee844eba977f7efa48dad16c76a
Ray Chung <ray.j.chung@gmail.com>
2020-11-13 19:44:10 -0800

    Add TicketUpdatedPublisher and update PUT /api/tickets .

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { validateRequest, NotAuthorizedError, NotFoundError, requireAuth } from '@rayjc-dev/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/TicketUpdatedPublisher';
+import { natsWrapper } from '../NatsWrapper';
 
 const router = express.Router();
 
@@ -29,6 +31,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     return res.json(ticket);
   });
