53e849857f74e64ccc04721181975992087dff8a
Ray Chung <ray.j.chung@gmail.com>
2021-01-07 23:04:13 -0800

    Add PaymentCreatedListener to orders.

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,6 +6,7 @@ import { natsWrapper } from './NatsWrapper';
 import { TicketCreatedListener } from './events/listeners/TicketCreatedListener';
 import { TicketUpdatedListener } from './events/listeners/TicketUpdatedListener';
 import { ExpirationCompleteListener } from './events/listeners/ExpirationCompleteListener';
+import { PaymentCreatedListener } from './events/listeners/PaymentCreatedListener';
 
 const init = async () => {
   try {
@@ -23,6 +24,7 @@ const init = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     // connect db
     await mongoose.connect(MONGODB_URI, {
